f1003070e0d134cb049f3f6e2de5452002bfed90
SirusCodes <darshandrander@gmail.com>
2020-10-13 17:08:22 +0530

    made add record dialog box

--- src/gui/landing_screen/LandingScreenController.java
+++ src/gui/landing_screen/LandingScreenController.java
@@ -1,4 +1,32 @@
 package gui.landing_screen;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class LandingScreenController {
+
+    public void addRecordClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("add_record_dialog.fxml"));
+        DialogPane dialogPane = fxmlLoader.load();
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setDialogPane(dialogPane);
+        dialog.setTitle("Add Record");
+        Optional<ButtonType> response = dialog.showAndWait();
+
+        if (response.isPresent())
+            if (response.get().equals(ButtonType.NEXT))
+                gotoAddRecordScreen();
+    }
+
+    public void gotoAddRecordScreen() {
+        System.out.println("NextScreen");
+    }
 }
