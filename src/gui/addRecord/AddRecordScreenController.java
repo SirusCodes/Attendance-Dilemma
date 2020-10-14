512c728211b0cc5b67d7714a641c4dd03a3aaac6
SirusCodes <darshandrander@gmail.com>
2020-10-14 19:13:50 +0530

    Made add record screen and back button implemented

--- src/gui/addRecord/AddRecordScreenController.java
+++ src/gui/addRecord/AddRecordScreenController.java
@@ -1,4 +1,27 @@
 package gui.addRecord;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddRecordScreenController {
+    @FXML
+    private Button backBtn;
+
+    public void backBtnClicked(ActionEvent event) throws IOException {
+        Stage stage = (Stage) backBtn.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("../landingScreen/landing_screen.fxml"));
+
+        Scene scene = new Scene(parent,960, 540);
+        stage.setScene(scene);
+        stage.setTitle("Attendance Dilemma");
+        stage.show();
+    }
+
 }
