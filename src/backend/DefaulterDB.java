84743344c2544d5f4649a86e02a94b5a6bd1726c
SirusCodes <darshandrander@gmail.com>
2020-11-17 21:28:57 +0530

    DefaulterDB solved 🥳🥳

--- src/backend/DefaulterDB.java
+++ src/backend/DefaulterDB.java
@@ -12,6 +12,7 @@ public class DefaulterDB {
     final private String uname = Auth.UNAME;
     final private String pass = Auth.PASSWORD;
     private Connection con;
+
     public DefaulterDB() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -23,7 +24,8 @@ public class DefaulterDB {
     public ArrayList<DefaulterModel> defaulterlist(int classID, Date startDate, Date endDate) {
         ArrayList<DefaulterModel> list = new ArrayList<>();
 
-        String query = "select s.student_name,(count(s.student_name)*100/c.no_of_lecture) as percentage from student s,date d,class c where d.date between " + startDate + " and " + endDate + " and c.class_id=" + classID + " and attendance like 'P' group by student_name having percentage<=75";
+        String query = "select s.student_name,(count(d.dstudent_id)*100/c.no_of_lecture) as percentage from date d,student s,class c where d.date between '" + startDate +
+                "' and '" + endDate + "' and s.sclass_id=" + classID + " and s.student_id=d.dstudent_id and c.class_id=s.sclass_id and attendance like 'A' group by s.student_name having percentage>25;";
 
         try {
             con = DriverManager.getConnection(url, uname, pass);
@@ -34,7 +36,7 @@ public class DefaulterDB {
             while (rs.next()) {
                 DefaulterModel defaulterModel = new DefaulterModel(
                         rs.getString("student_name"),
-                        rs.getDouble("percentage")
+                        100 - rs.getDouble("percentage")
                 );
                 list.add(defaulterModel);
             }
