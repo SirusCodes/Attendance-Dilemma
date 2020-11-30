a40d7c14041e2250db45a7208b900c5ab891006c
SirusCodes <darshandrander@gmail.com>
2020-11-30 11:55:21 +0530

    Using CSVReader

--- src/io/ReadRecord.java
+++ src/io/ReadRecord.java
@@ -1,33 +1,47 @@
 package io;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 import models.StudentRawModel;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class ReadRecord {
     public ArrayList<StudentRawModel> readFile(String fileAddress) {
         ArrayList<StudentRawModel> studentRawModelArrayList = new ArrayList<>();
-        String line;
-        Locale.setDefault(Locale.ENGLISH);
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(fileAddress));
-            while ((line = br.readLine()) != null) {
-                String[] split = line.split("\"");
-                String dateStr = split[1];
-                String[] student = split[0].split(",");
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/dd/yyyy, hh:mm:ss a");
-                LocalDateTime date = LocalDateTime.parse(dateStr, formatter);
-                StudentRawModel model = new StudentRawModel(student[0], student[1], date);
+            FileReader fileReader = new FileReader(fileAddress);
+
+            CSVParser parser = new CSVParserBuilder().withSeparator('\t').build();
+
+            CSVReader csv = new CSVReaderBuilder(fileReader)
+                    .withSkipLines(2)
+                    .withCSVParser(parser)
+                    .build();
+
+            List<String[]> data = csv.readAll();
+
+//            Locale.setDefault(Locale.ENGLISH);
+            for (String[] row : data) {
+                System.out.println(row[2]);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy, hh:mm:ss a", Locale.US);
+                String dateStr = row[2];
+                LocalDateTime date = LocalDateTime.parse(dateStr.trim(), formatter);
+                StudentRawModel model = new StudentRawModel(row[0], row[1], date);
                 studentRawModelArrayList.add(model);
             }
-        } catch (IOException e) {
+
+        } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
         return studentRawModelArrayList;
